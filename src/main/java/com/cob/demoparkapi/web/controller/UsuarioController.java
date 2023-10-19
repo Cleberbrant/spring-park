8184142b88ba774532a0d7526e378ba75aa64729
Cleberbrant <cleberobb@hotmail.com>
2023-10-19 09:59:25 -0300

    Feat: Add DTO architetury in Usuario

--- src/main/java/com/cob/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/cob/demoparkapi/web/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.cob.demoparkapi.web.controller;
 
 import com.cob.demoparkapi.entity.Usuario;
 import com.cob.demoparkapi.service.UsuarioService;
+import com.cob.demoparkapi.web.dto.UsuarioCreateDto;
+import com.cob.demoparkapi.web.dto.UsuarioResponseDto;
+import com.cob.demoparkapi.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +20,9 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto){
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
---
+++ src/main/java/com/cob/demoparkapi/web/dto/UsuarioResponseDto.java
@@ -0,0 +1,11 @@
+package com.cob.demoparkapi.web.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UsuarioResponseDto {
+
+    private Long id;
+    private String username;
+    private String role;
+}
